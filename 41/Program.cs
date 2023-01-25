6d2e0d1e31a0fedc0427ae134b24a912bef6feb3
Alexey <alex3122007@yandex.ru>
2023-01-25 11:39:39 +0300

    DZ

---
+++ 41/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.WriteLine("Введите числа через запятую: ");
+int[] arrayNumber = Console.ReadLine().Split(",").Select(x => int.Parse(x)).ToArray();
+int count =0;
+    for (int i =0; i<arrayNumber.Length;i++){
+        if (arrayNumber[i]> 0) count++;
+    }
+Console.WriteLine($"Больше нуля: {count}");
---
+++ 43/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+String [] arrayName = {"k1", "b1", "k2", "b2"};
+double[] arrayData = new double[arrayName.Length];
+
+for (int i=0; i< arrayName.Length; i++ ){
+Console.WriteLine($"Введите коэффициент {arrayName[i]}: ");
+    arrayData[i]= Convert.ToInt32(Console.ReadLine());
+}
+
+ double   x = (arrayData[3]-arrayData[1])/(arrayData[0]-arrayData[2]);
+ double   y = arrayData[0] * x +arrayData[1];
+
+Console.WriteLine($"Введите коэффициент ({x}; {y}) ");
